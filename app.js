ab7dea5da3f8b5b02873b38b1ae8bc4ba6ec674c
Rahul Rajput <rahulrajput3621@gmail.com>
2025-09-29 13:58:57 +0530

    Clean Code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-// const moment = require('moment-timezone');
 const mongoose = require('mongoose');
 const uri = 'mongodb://localhost:27017/commentApp';
 
@@ -33,23 +32,6 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-// let comments = [
-//     {
-//         id: uuid(),
-//         time: new Date(),
-//         user: "Rahul",
-//         text: "Comment jdinrrjf",
-//     },
-//     {
-//         id: uuid(),
-//         time: new Date(),
-//         user: "Himank",
-//         text: "Comment  cgyefdyuhwe ytfgweghcveufbc",
-//     },
-// ];
-
-
-
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -73,8 +55,6 @@ app.post('/data/new', async (req, res) => {
         text: text,
     })
     console.log('Comment Added: ', newComment)
-    // return res.status(201).json({msg: 'success'})
-    // comments.push({ id: uuid(), time: new Date(), user: user, text: text });
     res.redirect('/data')
 })
 
@@ -82,23 +62,18 @@ app.get('/data/show/:id', async (req, res) => {
     const { id } = req.params;
     const comment = await commentModel.findById(id);
     if (!comment) return res.status(404).json({ msg: 'Comment not Found!' });
-
-    // const comment = comments.find((x) => x.id === (id))
     res.render('show', { comment })
 })
 
 app.get('/data/edit/:id', async (req, res) => {
     const { id } = req.params;
     const comment = await commentModel.findById(id);
-    // const comment = comments.find((x) => x.id === (id))
     res.render('edit', { comment }, err = null)
 })
 app.post('/data/edit/:id', async (req, res) => {
     const { id } = req.params;
-    // const comment = await commentModel.findById(id);
     const { username, text } = req.body;
     if (!username) {
-        // res.render('edit',{ err: 'Username is required !!!' });
         res.redirect(`/data/edit/${id}`);
     }
     else {
@@ -108,9 +83,6 @@ app.post('/data/edit/:id', async (req, res) => {
             updateComment,
         )
         console.log('Updated Comment: ', commentUpdated);
-        // comments.find((x) => x.id === (id)).user = user;
-        // comments.find((x) => x.id === (id)).text = text;
-        // comments.find((x) => x.id === (id)).time = new Date();
         res.redirect('/data')
     }
 })
@@ -119,7 +91,6 @@ app.get('/data/delete/:id', async (req, res) => {
     const { id } = req.params;
     const deleted = await commentModel.findByIdAndDelete(id);
     console.log('Deleted Comment:', deleted);
-    // comments = comments.filter((comment) => comment.id !== id);
     res.redirect('/data');
 })
 
